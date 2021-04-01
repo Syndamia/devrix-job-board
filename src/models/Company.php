1787135de8e42d1319f72f4f4b2798aaf8b34c10
Syndamia <kamen.d.mladenov@protonmail.com>
2021-04-01 12:23:17 +0300

    Added a Company model

---
+++ src/models/Company.php
@@ -0,0 +1,43 @@
+<?php
+include_once("DBModel.php");
+
+class Company extends DBModel {
+	public const TABLE_NAME = "companies";
+	public const TABLE_COLUMNS = array(
+		"name " . DB::STRING_TYPE,
+	);
+
+	public string $name;
+
+	function __construct(string $name) {
+		$this->name = $name;
+	}
+
+	static function __constructStatic() {
+		self::$DB->createTable(self::TABLE_NAME, self::TABLE_COLUMNS);
+	}
+
+	/* Create */
+
+	/**
+	 * Inserts the giveb JobOffer into the databse. The id is automatically generated in the database.
+	 *
+	 * @param Company $com
+	 */
+	static function insertIntoDB($com) {
+		self::$DB->insertValue(self::TABLE_NAME, $com->name);
+	}
+
+	/* Read */
+
+	/**
+	 * Returns all Companies from the database.
+	 *
+	 * @return Company[]
+	 */
+	static function getAllFromDB() {
+		return self::$DB->getAllValues(self::TABLE_NAME);
+	}
+}
+
+Company::__constructStatic();
